d99ac3c46c3588d970acab7c2697e5a2d0c92bcd
Bolorino95 <angel.lopezbolorino@gmail.com>
2022-11-24 08:54:26 +0100

    24/11/2022

---
+++ SiloCreativo/JS/SiloCreativo.js
@@ -0,0 +1,29 @@
+
+
+
+const icono_burger = document.getElementById("burger");
+// console.log(icono_burger);
+
+const menu = document.querySelector(".menu");
+
+icono_burger.addEventListener("click", 
+      function(){
+
+      // console.log(menu.classList);
+       // menu.classList.add("nuevo")
+       // menu.classList.remove("menu")
+       // console.log(menu.classList);
+       //  console.log(menu.classList.contains("nuevo"));
+
+       if (menu.classList.contains("ocultar") == true){
+         menu.classList.remove("ocultar")
+         menu.classList.add("mostrar")
+       }
+       else if (menu.classList.contains("mostrar") == true){
+           menu.classList.remove("mostrar")
+           menu.classList.add("ocultar")
+       }
+
+       // div.classList.replace("ocultar", "mostrar");    
+       // div.classList.replace("mostrar", "ocultar"); 
+     });
